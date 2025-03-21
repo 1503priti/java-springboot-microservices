4cf27aaba8709df6cfee18942ccbaa00bf81ebb4
Priti Kumari <151085priti@gmail.com>
2025-03-21 09:34:07 +0800

    delete endpont added

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -45,7 +45,7 @@ public class PatientService {
         Patient patient = patientRepository.findById(id).orElseThrow(
                 () -> new PatientNotFoundException("Patient not found with ID: " + id));
 
-        /*if (patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+    /*if (patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
             throw new EmailAlreadyExistsException(
                     "A patient with this email " + "already exists"
                             + patientRequestDTO.getEmail());
@@ -65,5 +65,7 @@ public class PatientService {
         Patient updatedPatient = patientRepository.save(patient);
         return PatientMapper.toDTO(updatedPatient);
     }
-
+    public void deletePatient(UUID id) {
+        patientRepository.deleteById(id);
+    }
 }
